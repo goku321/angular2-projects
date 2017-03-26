450c5192a1048a51ec57a43dc21b729930cb4894
Deepak Sah <magnum-opus@localhost.localdomain>
2017-03-26 22:10:10 +0530

    data binding component added

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent {
+
+}
